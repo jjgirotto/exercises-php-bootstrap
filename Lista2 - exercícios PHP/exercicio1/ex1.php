d094ef88b66edf9c09bba2baf85021518d915013
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-24 19:59:17 -0300

    .gitignore e navbar

--- Lista2 - exercícios PHP/exercicio1/ex1.php
+++ Lista2 - exercícios PHP/exercicio1/ex1.php
@@ -6,25 +6,31 @@
     <title>Exercício 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body class="container">
-    <h2 class="mt-2">Exercício 1</h2>
-    <form action="ex1resp.php" method="POST">
-        <div class="row mt-3">
-            <div class="col">
-                <label for="valor1" class="form-label">Informe o valor 1: </label>
-                <input type="number" class="form-control" name="valor1" id="valor1">
+  <body>
+    <?php
+      require_once("../nav.php");
+      #include("../nav.php"); inclui mas roda o código debaixo normal
+    ?>
+    <main class="container">
+      <h2 class="mt-2">Exercício 1</h2>
+        <form action="ex1resp.php" method="POST">
+            <div class="row mt-3">
+                <div class="col">
+                    <label for="valor1" class="form-label">Informe o valor 1: </label>
+                    <input type="number" class="form-control" name="valor1" id="valor1">
+                </div>
+                <div class="col">
+                    <label for="valor2" class="form-label">Informe o valor 2: </label>
+                    <input type="number" class="form-control" name="valor2" id="valor2">
+                </div>
             </div>
-            <div class="col">
-                <label for="valor2" class="form-label">Informe o valor 2: </label>
-                <input type="number" class="form-control" name="valor2" id="valor2">
+            <div class="row mt-3">
+                <div class="col">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </div>
             </div>
-        </div>
-        <div class="row mt-3">
-            <div class="col">
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </div>
-        </div>
-    </form>
+        </form>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
